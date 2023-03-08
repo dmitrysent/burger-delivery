dd8afbb4d123af4c5e4a188cefae3c3912ea4f11
Dmitry Sentyurev <dmitry.sent@gmail.com>
2023-03-08 20:44:49 +0300

    add github actions

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import Container from "./components/Container/Container";
 import Header from "./components/Header/Header";
 
 export const App = () => {
-  return (
-      <Header />
-  )
+    return (
+        <>
+            <Header/>
+            <Header/>
+        </>
+    )
 }
